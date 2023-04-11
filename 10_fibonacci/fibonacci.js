1ea8a9c758259cc34e518a3c9b84e8574de96bfd
Joel Hemmelgarn <jdhemmelgarn@gmail.com>
2023-04-11 11:21:49 -0400

    Fibonacci Problem solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
 
+    let input = parseInt(num);
+    let fibArr = [0, 1];
+
+
+    for (let i = 2; i <= input; i++) {
+        fibArr[i] = fibArr[i - 2] + fibArr[i - 1];
+    }
+
+    return fibArr[input];
 };
 
 // Do not edit below this line
